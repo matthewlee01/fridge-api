a2031e091e9f6038943d23c1d5944aa3b9fbc035
Matthew Lee <laughew@gmail.com>
2021-01-09 17:15:41 -0800

    initialized schema

---
+++ models/elementModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const ElementSchema = new mongoose.Schema({
+	type: {
+		type: String,
+		lowercase: true,
+		required: true
+	}
+	x: {
+		type: Number,
+		required: true
+	}
+	y: {
+		type: Number,
+		required: true
+	}
+	value: {
+		type: String,
+		default: "no value"
+	}
+});
+
+const Element = mongoose.model("Element", ElementSchema);
+module.exports = Element;
