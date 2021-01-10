c2f6bafc8058bca48966d1481e88e011a37ebcf8
Matthew Lee <laughew@gmail.com>
2021-01-09 20:28:30 -0800

    added cors middleware

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const elementRouter = require('./routes/elementRoutes.js');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, {
